a93304c3a323dad9178dfeee995a8dadd3eba8db
giuse <giuseppedefilippo03@gmail.com>
2025-01-23 15:31:39 +0100

    adjust name

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,6 +1,6 @@
 const connection = require("../connection");
 function index(req, res) {
-    const sql = "SELECT * FROM `books`";
+    const sql = "SELECT * FROM `movies`";
     connection.query(sql, (err, results) => {
         if (err) {
             console.error("errore non va ", err);
@@ -14,7 +14,7 @@ function index(req, res) {
 }
 function show(req, res) {
     const id = parseInt(req.params.id);
-    const sql = "SELECT * FROM `posts` WHERE `id` = ?";
+    const sql = "SELECT * FROM `movies` WHERE `id` = ?";
     connection.query(sql, [id], (err, results) => {
         if (err) return res.status(500).json({ error: "database query failed" });
         const item = results[0];
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
+const bookRouter = require("./routers/bookRouter")
 // const bookRouter = require("./routers/bookRouter");
 const PORT = process.env.port || 3000;
 
@@ -15,7 +16,7 @@ app.get("/", (req, res) => {
     console.log('dsa');
     res.send('Hello World!')
 });
-// app.use("/api/post", postRouter);
+app.use("/api/movie", bookRouter);
 
 app.listen(PORT, () => {
     console.log(`Server in esecuzione su http://localhost:${PORT}`);
